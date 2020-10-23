55e2ef170c4e6cb1d03673d53b8188894f83a76b
ChernomorYegor <chernomor.yegor@gmail.com>
2020-10-23 17:28:20 +0300

    feat: implement function that reverse digits

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    const strN = n.toString();
+    let result = '';
+
+    for (let i = strN.length - 1; i >= 0; i--) {
+        result += strN[i];
+    }
+
+    return parseInt(result);
 }
